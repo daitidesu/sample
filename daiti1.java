c836d29430ac1bb4e8632339b13fc824d99dcd44
daitidesu <daiti0228neko@outlook.jp>
2021-05-06 22:04:58 +0900

    修正

--- daiti1.java
+++ daiti1.java
@@ -2,7 +2,7 @@ class daiti1{
   public static void main(String[] args) {
     System.out.println("hello world.");  
     for(int i=0;i<10;i++){
-      System.out.println(i+"loop")
+      System.out.println(i+"loop");
     }
   }
 }
